d1b5b70da4d508bc3181a4535e75378de531e6b7
Ionut-Cora <cora.ionut92@yahoo.com>
2021-06-13 00:49:34 +0100

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,11 +11,11 @@ function Header() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/buy">Buy</Nav.Link>
-                        <Nav.Link href="/rent">Rent</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
+                        <Nav.Link href="/#/">Home</Nav.Link>
+                        <Nav.Link href="/#/buy">Buy</Nav.Link>
+                        <Nav.Link href="/#/rent">Rent</Nav.Link>
+                        <Nav.Link href="/#/about">About</Nav.Link>
+                        <Nav.Link href="/#/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </div>
