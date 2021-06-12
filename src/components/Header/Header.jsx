e589cec37cb4b7acafbb64a56d2f5c5522522234
Ionut-Cora <cora.ionut92@yahoo.com>
2021-06-13 00:52:01 +0100

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,15 +7,15 @@ function Header() {
         <div>
             <Navbar bg="dark" variant="dark" expand="lg">
                 <div className="container d-flex">
-                    <Navbar.Brand href="/" id="logo">Koras</Navbar.Brand>
+                    <Navbar.Brand href="/koras-estates-agency/#/" id="logo">Koras</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
-                        <Nav.Link href="/#/">Home</Nav.Link>
-                        <Nav.Link href="/#/buy">Buy</Nav.Link>
-                        <Nav.Link href="/#/rent">Rent</Nav.Link>
-                        <Nav.Link href="/#/about">About</Nav.Link>
-                        <Nav.Link href="/#/contact">Contact</Nav.Link>
+                        <Nav.Link href="/koras-estates-agency/#/">Home</Nav.Link>
+                        <Nav.Link href="/koras-estates-agency/#/buy">Buy</Nav.Link>
+                        <Nav.Link href="/koras-estates-agency/#/rent">Rent</Nav.Link>
+                        <Nav.Link href="/koras-estates-agency/#/about">About</Nav.Link>
+                        <Nav.Link href="/koras-estates-agency/#/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </div>
