45eea440419bc1c363280593ec14c8935fe4484b
Ionut-Cora <cora.ionut92@yahoo.com>
2021-06-13 01:20:57 +0100

    commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
     return(
         <div>
-            <Navbar bg="dark" variant="dark" expand="lg">
                 <div className="container d-flex">
-                    <Navbar.Brand to="/" id="logo">Koras</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                        <Nav.Link to="/">Home</Nav.Link>
-                        <Nav.Link to="/buy">Buy</Nav.Link>
-                        <Nav.Link to="/rent">Rent</Nav.Link>
-                        <Nav.Link to="/about">About</Nav.Link>
-                        <Nav.Link to="/contact">Contact</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
+                    <Link to="/" id="logo">Koras</Link>
+                    <Link to="/">Home</Link>
+                    <Link to="/buy">Buy</Link>
+                    <Link to="/rent">Rent</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
                 </div>
-            </Navbar>
         </div>
     )
 }
