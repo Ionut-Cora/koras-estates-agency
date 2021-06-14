72ee1ffae97c7ad4db9461483713383fd443dd3e
Ionut-Cora <cora.ionut92@yahoo.com>
2021-06-14 14:14:43 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter hashType="noslash">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
