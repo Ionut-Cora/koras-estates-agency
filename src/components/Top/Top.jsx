e5dd63467efa74a8af2ff7963d554c46f41cd7fa
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-15 15:45:44 +0100

    top info section style fixed

--- src/components/Top/Top.jsx
+++ src/components/Top/Top.jsx
@@ -3,15 +3,15 @@ import './Top.css';
 
 function Top() {
     return(
-        <div className="container" id="top">
-            <div id="top-phone">
+        <div className="container d-flex align-items-center justify-content-between" id="top">
+            <div className='d-flex align-items-center' id="top-phone">
                 <ion-icon name="call-outline"></ion-icon>
-                <p>02030304040</p>
+                <p className='mb-0 mx-2'>02030304040</p>
             </div>
             <div id="top-other">
-                <ion-icon name="logo-facebook"></ion-icon>
-                <ion-icon name="mail-outline"></ion-icon>
-                <ion-icon name="location-outline"></ion-icon>
+                <span className='mx-1'><ion-icon name="logo-facebook"></ion-icon></span>
+                <span className='mx-1'><ion-icon name="mail-outline"></ion-icon></span>
+                <span className='mx-1'><ion-icon name="location-outline"></ion-icon></span>               
             </div>
         </div>
     )
