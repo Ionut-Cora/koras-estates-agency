cb1c93ef73ce0ce9a7369e37eb150e8ba8509597
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-17 16:23:56 +0100

    rent a property section style fixed

--- src/components/PropertiesSection/PropertiesSection.jsx
+++ src/components/PropertiesSection/PropertiesSection.jsx
@@ -9,7 +9,7 @@ function PropertiesSection() {
     return(
         <div className="properties-section container py-5">
             <div className="d-flex flex-column">
-                <div className="properties-item d-flex align-items-center flex-column-reverse flex-lg-row mb-5 mb-lg-0 p-0 px-lg-5 py-lg-3 p-xl-5">
+                <div className="properties-item d-flex align-items-center flex-column-reverse flex-lg-row mb-5 mb-lg-0 p-0 px-lg-5 py-lg-3 px-xl-5 pb-xl-5 pt-xl-0">
                     <img className='shadow' src={rentPicture} alt="rent properties"/>
                     <div className="properties-details d-flex flex-column align-items-center justify-content-center px-3 pt-0 pb-5 p-lg-5 ">
                         <h2 className='text-capitalize'>
@@ -28,7 +28,7 @@ function PropertiesSection() {
                         </h2>
                         <p className='mb-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi nemo nobis minima atque sunt iure, ullam, fugit nam rerum soluta dolores! Natus magni laudantium consequuntur quia, iusto earum officia aperiam?</p>
                         <Link to="/buy">
-                            <Button className='m-2 my-md-0 rounded-0 text-uppercase shadow' variant='outline-light' size='lg'>Buy a property</Button>                       
+                            <Button className='m-2 my-md-0 rounded-0 text-uppercase shadow' variant='light' size='lg'>Buy a property</Button>                       
                         </Link>
                     </div>
                     <img className='shadow' src={buyPicture} alt="buy properties"/>
