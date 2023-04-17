a8ac5354a8ef2457c5b7d297668448a0c10c9ee3
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-17 17:52:45 +0100

    mortgage calculator component style change

--- src/components/MortgageCalculator/MortgageCalculator.jsx
+++ src/components/MortgageCalculator/MortgageCalculator.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './MortgageCalculator.css';
 
 class MortgageCalculator extends React.Component {
@@ -9,7 +10,7 @@ class MortgageCalculator extends React.Component {
             deposit: '',
             interest: '',
             years: '',
-            total: ''
+            total: '0'
         }
     }
 
@@ -50,23 +51,38 @@ class MortgageCalculator extends React.Component {
 
     render() {
         return(
-            <div className="mortgage-section container">
-                <h2>Mortgage Calculator</h2>
-                <form id="calculator-form" onSubmit={(event) => this.handleTotal(event)} >
-                    <label htmlFor="full-price">Full price</label>
-                    <input className="calculator-input" type="number" name="full-price" onChange={(event) => this.handleFullPrice(event)} required />
-                    <label htmlFor="deposit">Deposit</label>
-                    <input className="calculator-input" type="number" name="deposit" onChange={(event) => this.handleDeposit(event)} required />
-                    <label htmlFor="interest">Interest</label>
-                    <input className="calculator-input" type="number" name="interest" onChange={(event) => this.handleInterest(event)} required />
-                    <label htmlFor="years">Years</label>
-                    <input className="calculator-input" type="number" name="years" onChange={(event) => this.handleYears(event)} required />
-                    <input id="calculator-submit" type="submit" />
-                </form>
-                <div id="calculator-result">
-                    <p>Total:</p>
-                    <p>£{this.state.total}</p>
-                </div>
+            <div className="mortgage-section container d-flex flex-column align-items-center py-5">
+                <h2 className='text-capitalize mb-3'>
+                    <strong>Mortgage calculator</strong>
+                </h2>
+                <p className='mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam dolore, saepe sit corporis ea enim pariatur nostrum debitis asperiores, quo facilis repellendus, quam iusto! Culpa illo nisi, eaque iusto ex odio, similique labore facilis molestias atque nemo qui. Obcaecati, libero.</p>
+                <form className='bg-dark text-light d-flex flex-column align-items-center p-5' id="calculator-form" onSubmit={(event) => this.handleTotal(event)} >
+                    <div className="mb-3 d-flex flex-column w-100">
+                        <label className='text-capitalize' htmlFor="full-price">Full price</label>
+                        <input className="w-100 border-0 px-3" type="number" name="full-price" placeholder='Ex: 470000' onChange={(event) => this.handleFullPrice(event)} required />
+                    </div>
+                    <div className="mb-3 d-flex flex-column w-100">
+                        <label htmlFor="deposit">Deposit</label>
+                        <input className="w-100 border-0 px-3" type="number" name="deposit" placeholder='Ex: 70500' onChange={(event) => this.handleDeposit(event)} required />
+                    </div>
+                    <div className="mb-3 d-flex flex-column w-100">
+                        <label htmlFor="interest">Interest</label>
+                        <input className="w-100 border-0 px-3" type="number" name="interest" placeholder='Ex: 3' onChange={(event) => this.handleInterest(event)} required />
+                    </div>
+                    <div className="mb-3 d-flex flex-column w-100">
+                        <label htmlFor="years">Years</label>
+                        <input className="w-100 border-0 px-3" type="number" name="years" placeholder='Ex: 35' onChange={(event) => this.handleYears(event)} required />
+                    </div>
+
+                    <Button id="calculator-submit" type="submit" className='rounded-0 border-0 text-uppercase mt-3 mb-4 shadow' variant='dark' size='lg'>Submit</Button>
+
+                    <div className='w-100 d-flex flex-column flex-md-row justify-content-center align-items-center p-4 text-dark' id="calculator-result">
+                        <p className='mx-1 mb-0'>Result:</p>
+                        <p className='mx-1 mb-0'>
+                            <strong>£{this.state.total}</strong> per month
+                        </p>
+                    </div>
+                </form>              
             </div>
         )
     }
