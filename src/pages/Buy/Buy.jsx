fb2424c875fa5300091bacb9ceaec055aced2463
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-18 12:36:19 +0100

    buy page cards style change

--- src/pages/Buy/Buy.jsx
+++ src/pages/Buy/Buy.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../../components/Layout/Layout';
 import products from '../../utils/products.json';
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 import MortgageCalculator from '../../components/MortgageCalculator/MortgageCalculator';
 import './BuyRent.css';
 
@@ -9,75 +10,88 @@ function Buy() {
     return(
         <div>
             <Layout>
-                <div className="buy-rent container">
-                    <h1>Properties to buy</h1>
-                    <div>
-                        {products.buy.map((product) => (
-                            <div key={product.id}>
-                                <div className="product-section d-flex flex-column align-items-center">
-                                    <div className="w-75">
-                                            <Carousel>
-                                                <Carousel.Item>
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={product.image1}
-                                                        alt="First slide"
-                                                    />
-                                                    <Carousel.Caption>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>
-                                                <Carousel.Item>
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={product.image2}
-                                                        alt="Second slide"
-                                                    />
+                <div className="buy-rent">
+                    <div className='container py-5'>
+                        <h1 className='text-capitalize mb-3 mb-md-5 mt-0 mt-md-2'>
+                            <strong>Properties for sale</strong>
+                        </h1>
+                        <div className='row'>
+                            {products.buy.map((product) => (
+                                <div className='col-12 col-lg-6 my-3' key={product.id}>
+                                    <Card className="product-section d-flex flex-column align-items-center h-100 shadow">
+                                        <div className="">
+                                                <Carousel slide={false} interval={null}>
+                                                    <Carousel.Item>
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={product.image1}
+                                                            alt="First slide"
+                                                        />
+                                                        <Carousel.Caption>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                    <Carousel.Item>
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={product.image2}
+                                                            alt="Second slide"
+                                                        />
 
-                                                    <Carousel.Caption>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>
-                                                <Carousel.Item>
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={product.image3}
-                                                        alt="Third slide"
-                                                    />
+                                                        <Carousel.Caption>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                    <Carousel.Item>
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={product.image3}
+                                                            alt="Third slide"
+                                                        />
 
-                                                    <Carousel.Caption>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>
-                                                <Carousel.Item>
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={product.image4}
-                                                        alt="Fourth slide"
-                                                    />
+                                                        <Carousel.Caption>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                    <Carousel.Item>
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={product.image4}
+                                                            alt="Fourth slide"
+                                                        />
 
-                                                    <Carousel.Caption>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>
-                                                <Carousel.Item>
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src={product.image5}
-                                                        alt="Fifth slide"
-                                                    />
+                                                        <Carousel.Caption>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                    <Carousel.Item>
+                                                        <img
+                                                            className="d-block w-100"
+                                                            src={product.image5}
+                                                            alt="Fifth slide"
+                                                        />
 
-                                                    <Carousel.Caption>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>
-                                            </Carousel>
-                                    </div>
-                                    <div className="product-details">
-                                        <h3>{product.address}</h3>
-                                        <p>{product.longdescription}</p>
-                                        <p>{product.price}</p>
-                                    </div>
+                                                        <Carousel.Caption>
+                                                        </Carousel.Caption>
+                                                    </Carousel.Item>
+                                                </Carousel>
+                                        </div>
+                                        <Card.Body className="product-details d-flex flex-column align-items-start p-4">
+                                            <Card.Text className='rooms mb-2'>
+                                                <strong>{product.rooms}</strong>
+                                            </Card.Text>
+                                            <Card.Title className='text-light mb-2'>
+                                                <strong>{product.price}</strong>
+                                            </Card.Title>
+                                            <Card.Text className='text-light address mb-4'>
+                                                <strong>{product.address}</strong>
+                                            </Card.Text>
+                                            <Card.Text className='text-light text-left'>{product.longdescription}</Card.Text>                                      
+                                        </Card.Body>
+                                    </Card>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
+
                     <MortgageCalculator />
+
                     <div className="interest-section">
                         <h4>Interested in one of the above properties?</h4>
                         <p>Call Koras</p>
