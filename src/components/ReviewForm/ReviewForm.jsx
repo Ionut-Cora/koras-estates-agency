c00042dd4c6e5904b4fc9d4b1d9be5c939473e7c
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-19 12:15:26 +0100

    reviews component style change

--- src/components/ReviewForm/ReviewForm.jsx
+++ src/components/ReviewForm/ReviewForm.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import './ReviewForm.css';
 
 class ReviewForm extends React.Component {
@@ -25,27 +27,74 @@ class ReviewForm extends React.Component {
 
         let list = document.getElementById('list');
         let newCustomer = document.createElement('li');
-        newCustomer.appendChild(document.createTextNode(this.state.name + ' - ' + this.state.comment));
+        // newCustomer.appendChild(document.createTextNode(this.state.name + ' - ' + this.state.comment));
+
+        newCustomer.className += 'col-12 col-lg-6 my-3';
+        newCustomer.innerHTML = `
+            <div class="card h-100 rounded-0">
+                <div class="card-body d-flex align-items-center justify-content-center">
+                    <p class="card-text text-light">` + this.state.name + ` - ` + this.state.comment + `</p>
+                </div>
+            </div>
+        `;
         list.appendChild(newCustomer);
     }
 
     render() {
         return(
-            <div id="review-section">
-                <h3>Reviews</h3>
-                <h4>What are our customers saying?</h4>
-                <ul id="list">
-                    <li>John Smith - The best estate agency in London !</li>
-                    <li>Emma Bower - They helped me to buy my own property. I recommend them !</li>
-                    <li>Mark Elbert - I rented a flat with their help and I'm very happy !</li>
-                </ul>
+            <div className='bg-dark py-5' id="review-section">
+                <div className="container">
+                    <h3 className='text-light text-capitalize mb-3'>
+                        <strong>What are our customers saying?</strong>
+                    </h3>
+                    <p className='text-light mb-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur iste doloribus praesentium fugiat est quas sapiente voluptas neque cupiditate consequuntur ad, placeat commodi! Expedita a nobis doloremque praesentium possimus cumque.</p>
+                    <ul className='row justify-content-center p-0 list-unstyled'>
+                        <li className='col-12 col-lg-6 my-3'>
+                            <Card className='h-100 rounded-0'>
+                                <Card.Body className='d-flex align-items-center justify-content-center'>
+                                    <Card.Text className='text-light'>John Smith - The best estate agency in London !</Card.Text>
+                                </Card.Body>
+                            </Card>                      
+                        </li>
+                        <li className='col-12 col-lg-6 my-3'>
+                            <Card className='h-100 rounded-0'>
+                                <Card.Body className='d-flex align-items-center justify-content-center'>
+                                    <Card.Text className='text-light'>Emma Bower - They helped me to buy my own property. I recommend them !</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li className='col-12 col-lg-6 my-3'>
+                            <Card className='h-100 rounded-0'>
+                                <Card.Body className='d-flex align-items-center justify-content-center'>
+                                    <Card.Text className='text-light'>Mark Elbert - I rented a flat with their help and I'm very happy !</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                        <li className='col-12 col-lg-6 my-3'>
+                            <Card className='h-100 rounded-0'>
+                                <Card.Body className='d-flex align-items-center justify-content-center'>
+                                    <Card.Text className='text-light'>Anna Gold - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, maxime.</Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </li>
+                    </ul>
 
-                <form id="customer" onSubmit={(event) => this.handleSubmit(event)}>
-                    <label id="name-text" htmlFor="name">Name:</label>
-                    <input id="name" type="text" name="name" onChange={(event) => this.handleName(event)} required />
-                    <input id="name-submit" type="submit"/>
-                </form>
-                <textarea id="customer-review" name="comment" form="customer" placeholder="Enter your review" cols="40" rows="5" onChange={(event) => this.handleComment(event)} required ></textarea>
+                    <div className='d-flex justify-content-center'>                  
+                        <form className='bg-light py-4 px-5' id="customer-form" onSubmit={(event) => this.handleSubmit(event)}>
+                            <div className='mb-3 d-flex flex-column'>
+                                <label className='text-capitalize' id="name-text" htmlFor="name">Your name</label>
+                                <input className='p-2' id="name" type="text" name="name" onChange={(event) => this.handleName(event)} required />
+                            </div>
+                            <div className='mb-3 d-flex flex-column'>
+                                <label className='text-capitalize' htmlFor="customer-review">Your review</label>
+                                <textarea className='p-2' id="customer-review" name="comment" form="customer" rows="3" onChange={(event) => this.handleComment(event)} required ></textarea>
+                            </div>
+                            <div>
+                                <Button className='rounded-0 border-0 text-uppercase' id="name-submit" type="submit" variant='dark' size='lg'>Submit</Button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -8,22 +8,24 @@ function About() {
     return(
         <div>
             <Layout>
-                <div className="about-page container py-5">
-                    <div className='row py-5' id="about">
-                        <div className="col-12 col-lg-6">
-                            <div className='d-flex flex-column align-items-center justify-content-center px-0 px-lg-5'>
-                                <h1 className='text-capitalize mb-3'>
-                                    <strong>About us</strong>
-                                </h1>
-                                <p className='text-left'>Founded in 2005, this small family business tries its best for its customers. We aim to expand in the coming years, to help as many people as possible who want to either rent a property or buy their own house.</p>
-                                <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium alias fugit illo molestias obcaecati, facilis reiciendis harum blanditiis. Ratione sequi mollitia ipsam consequatur dolorum reiciendis qui delectus soluta voluptates. Minus.</p>
+                <div className="about-page">
+                    <div className="container py-5">
+                        <div className='row pt-5' id="about">
+                            <div className="col-12 col-lg-6">
+                                <div className='d-flex flex-column align-items-center justify-content-center px-0 px-lg-5'>
+                                    <h1 className='text-capitalize mb-3'>
+                                        <strong>About us</strong>
+                                    </h1>
+                                    <p className='text-left'>Founded in 2005, this small family business tries its best for its customers. We aim to expand in the coming years, to help as many people as possible who want to either rent a property or buy their own house.</p>
+                                    <p className='text-left'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium alias fugit illo molestias obcaecati, facilis reiciendis harum blanditiis. Ratione sequi mollitia ipsam consequatur dolorum reiciendis qui delectus soluta voluptates. Minus.</p>
+                                </div>
                             </div>
+                            <div className="col-12 col-lg-6">
+                                <div className='h-100 d-flex align-items-center justify-content-center px-0 px-lg-5'>
+                                    <img className='w-100 mt-4 mt-lg-0' src={AboutImg} alt="about img" />
+                                </div>
+                            </div>                       
                         </div>
-                        <div className="col-12 col-lg-6">
-                            <div className='h-100 d-flex align-items-center justify-content-center px-0 px-lg-5'>
-                                <img className='w-100 mt-4 mt-lg-0' src={AboutImg} alt="about img" />
-                            </div>
-                        </div>                       
                     </div>
 
                     <ReviewForm />
