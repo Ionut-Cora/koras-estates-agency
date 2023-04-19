f6432fdbcb03ef72f395ed9b8b93db42cdb055fe
Ionut Cora <cora.ionut92@yahoo.com>
2023-04-19 12:55:43 +0100

    contact page style change

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,22 +7,35 @@ function Contact() {
     return(
         <div>
             <Layout>
-                <div className="contact container">
-                    <div id="office-image-div">
-                        <img id="office-image" src={office} alt="office"/>
-                    </div>
-                    <div id="office-details-div">
-                        <h1>Contact us</h1>
-                        <div id="contact-page-phone">
-                            <ion-icon name="call-outline"></ion-icon>
-                            <a href="tel:020 3030 4040">020 3030 4040</a>
+                <div className="contact container my-5">
+                    <div className="row flex-column-reverse flex-lg-row h-100 pt-5">
+                        <div className='col-12 col-lg-6 d-flex align-items-center justify-content-center'>
+                            <img className='shadow' src={office} alt="office"/>
                         </div>
-                        <div id="contact-page-email">
-                            <ion-icon name="mail-outline"></ion-icon>
-                            <a href="mailto:koras.agency@gmail.com">koras.agency@gmail.com</a>
+                        <div className='col-12 col-lg-6 d-flex flex-column align-items-center justify-content-center mb-5 mb-lg-0'>
+                            <h1 className='text-capitalize mb-3'>
+                                <strong>Contact us</strong>
+                            </h1>
+                            <p className='text-left mb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil perferendis dolore ad porro praesentium aperiam numquam quia voluptates, atque consectetur facilis aspernatur vero accusantium ipsum beatae nostrum expedita sequi aut!</p>
+                            <div className='mb-2'>
+                                <span className='mx-1'>
+                                    <ion-icon name="call-outline"></ion-icon>
+                                </span>
+                                <a className='mx-1 text-dark' href="tel:020 3030 4040">020 3030 4040</a>
+                            </div>
+                            <div className='mb-2'>
+                                <span className='mx-1'>
+                                    <ion-icon name="mail-outline"></ion-icon>
+                                </span>
+                                <a className='mx-1 text-dark' href="mailto:koras.agency@gmail.com">koras.agency@gmail.com</a>
+                            </div>
+                            <div className='d-flex'>
+                                <span className='mx-1'>
+                                    <ion-icon name="location-outline"></ion-icon>
+                                </span>
+                                <p className='mx-1'>1 Street Name, London FA1 1LO</p>
+                            </div>
                         </div>
-                        <ion-icon name="location-outline"></ion-icon>
-                        <p>1 Fake Street London FA1 1LO</p>
                     </div>
                 </div>
             </Layout>
